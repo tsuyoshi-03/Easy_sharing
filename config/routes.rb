df0dacf99b0983086c467692db1312f29655e026
tsuyoshi <zynb0316@icloud.com>
2021-04-09 08:23:23 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root "pages#index"
+  get 'sessions/new'
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/index'
+  get 'pages/top'
+  get 'pages/search'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
+  resources :users
+  resources :topics
+end
