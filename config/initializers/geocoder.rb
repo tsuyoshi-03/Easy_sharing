91d932c3ce9dc4a7ff8a3a29fb0dd8141d038932
tsuyoshi <zynb0316@icloud.com>
2021-05-02 05:21:41 +0000

    0502

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
    use_https: true,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-   api_key: "",               # API key for geocoding service
+   api_key: "AIzaSyD9G1-Mx1yKDMSQIAy95Am4RGF63Dwkh6k",               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
