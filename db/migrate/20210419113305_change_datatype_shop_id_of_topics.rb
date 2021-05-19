97a986deaeff79dbed56eaa1bcb07d6baf739c11
tsuyoshi <zynb0316@icloud.com>
2021-05-19 09:31:21 +0000

    db修正(change_datatype_shop_id_of_topics.rb)

--- db/migrate/20210419113305_change_datatype_shop_id_of_topics.rb
+++ db/migrate/20210419113305_change_datatype_shop_id_of_topics.rb
@@ -1,5 +1,9 @@
 class ChangeDatatypeShopIdOfTopics < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :topics, :shop_id, :integer
+  end
+  
+  def down
+    change_column :topics, :shop_id, :string
   end
 end
